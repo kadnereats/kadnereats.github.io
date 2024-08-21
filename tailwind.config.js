6d94845c902ffd8dac4f2c23ed6a58897dc2a440
kadnereats <early2rager0@gmail.com>
2024-08-20 21:54:01 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  darkMode: 'class', // or 'media' or 'class'
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazir: ['Vazir'],
+        spacemono: ['Space Mono'],
+        montserrat: ['Montserrat']
+      },
+      padding: {
+        'right-16': '16px',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant, e }) {
+      addVariant('rtl', ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.rtl .${e(`rtl${separator}${className}`)}`
+        },
+          {
+            variants: ['responsive'],
+          })
+      })
+    }),
+  ],
+}
